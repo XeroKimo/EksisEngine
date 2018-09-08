02aae7fecdcab9c3c59da47af4b5ec5f61c71ec1
XeroKimo <38732789+XeroKimo@users.noreply.github.com>
2018-09-08 14:24:04 -0400

    Add files via upload

--- EksisEngine/EksisEngine/EMath.h
+++ EksisEngine/EksisEngine/EMath.h
@@ -5,12 +5,6 @@
 
 struct EMath
 {
-	static int SquareToSquareDetection(EVector position1, EVector position2, EVector size1, EVector size2);
-	static float CorrectHitBox(EVector position1, EVector position2, EVector size1, EVector size2);
-	static float xAxisCheck(EVector position1, EVector position2, EVector size1, EVector size2);
-	static float yAxisCheck(EVector position1, EVector position2, EVector size1, EVector size2);
-
-private:
-	static int axisCheck(EVector position1, EVector position2, EVector size1, EVector size2);
-	static int lowestValue(int values[], int size);
+	static bool SquareToSquareDetection(EVector position1, EVector position2, EVector size1, EVector size2);
+	static EVector CorrectHitBox(EVector position1, EVector position2, EVector size1, EVector size2);
 };
